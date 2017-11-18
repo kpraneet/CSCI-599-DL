d6bce214668a995a02ea3a7d59fbb2623169c983
Praneet Kalluri <praneet97@gmail.com>
2017-11-18 02:22:59 -0800

    Pre process test. Needs more data. Not efficient, use just for test. Have to modify for final use.

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,7 +1,53 @@
+import os
+import cv2
+import numpy as np
 from keras import optimizers
 from keras.models import Sequential, load_model
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.layers import Dense, Dropout, BatchNormalization, Flatten, Conv2D, MaxPooling2D, GlobalMaxPool2D
+
+
+def pre_process():
+    sax = "/Users/praneet/Documents/USC - MS/Fall 2017/CS 599 - DL/CSCI-599-DL/IRMAS-spectrograms/Training/sax"
+    vio = "/Users/praneet/Documents/USC - MS/Fall 2017/CS 599 - DL/CSCI-599-DL/IRMAS-spectrograms/Training/vio"
+    train_data = []
+    train_label = []
+    eval_data = []
+    eval_label = []
+    test_data = []
+    test_label = []
+    for root, dirs, files in os.walk(sax):
+            for file_name in files:
+                if file_name.endswith(".png"):
+                    file_path = os.path.abspath(os.path.join(root, file_name))
+                    img = cv2.imread(file_path)
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    fixed_size = (43, 128)
+                    img = cv2.resize(img, dsize=fixed_size)
+                    train_data.append(img)
+                    train_label.append(0)
+    for root, dirs, files in os.walk(vio):
+            for file_name in files:
+                if file_name.endswith(".png"):
+                    file_path = os.path.abspath(os.path.join(root, file_name))
+                    img = cv2.imread(file_path)
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    fixed_size = (43, 128)
+                    img = cv2.resize(img, dsize=fixed_size)
+                    train_data.append(img)
+                    train_label.append(1)
+    eval_data.append(train_data[0])
+    eval_label.append(train_label[0])
+    test_data.append(train_data[1])
+    test_label.append(train_label[1])
+    train_data = np.array(train_data, dtype=np.float32) / 255. #, dtype=np.float32)
+    train_label = np.array(train_label, dtype=np.int32) #, dtype=np.int32)
+    eval_data = np.array(eval_data) #, dtype=np.float32)
+    eval_label = np.array(eval_label) #, dtype=np.int32)
+    test_data = np.array(test_data) #, dtype=np.float32)
+    test_label = np.array(test_label) #, dtype=np.int32)
+    print(train_data.shape)
+    return train_data, train_label, eval_data, eval_label, test_data, test_label
 
 
 def main():
@@ -10,6 +56,8 @@ def main():
     eval_data = None
     eval_label = None
     test_data = None
+    test_label = None
+    train_data, train_label, eval_data, eval_label, test_data, test_label = pre_process()
     epochs = 10
     learn_rate = 0.001
     model = Sequential()
