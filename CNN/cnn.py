9fb30d63a376d69dd4c63210ff73f326f550efb6
Praneet Kalluri <praneet97@gmail.com>
2017-11-24 20:00:47 -0800

    Latest version of CNN.

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,106 +1,70 @@
-import os
-import cv2
+import os.path
 import numpy as np
 from keras import optimizers
 from keras.models import Sequential, load_model
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.layers import Dense, Dropout, BatchNormalization, Flatten, Conv2D, MaxPooling2D, GlobalMaxPool2D
-
-
-def pre_process():
-    sax = "/Users/praneet/Documents/USC - MS/Fall 2017/CS 599 - DL/CSCI-599-DL/IRMAS-spectrograms/Training/sax"
-    vio = "/Users/praneet/Documents/USC - MS/Fall 2017/CS 599 - DL/CSCI-599-DL/IRMAS-spectrograms/Training/vio"
-    train_data = []
-    train_label = []
-    eval_data = []
-    eval_label = []
-    test_data = []
-    test_label = []
-    for root, dirs, files in os.walk(sax):
-            for file_name in files:
-                if file_name.endswith(".png"):
-                    file_path = os.path.abspath(os.path.join(root, file_name))
-                    img = cv2.imread(file_path)
-                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    fixed_size = (43, 128)
-                    img = cv2.resize(img, dsize=fixed_size)
-                    train_data.append(img)
-                    train_label.append(0)
-    for root, dirs, files in os.walk(vio):
-            for file_name in files:
-                if file_name.endswith(".png"):
-                    file_path = os.path.abspath(os.path.join(root, file_name))
-                    img = cv2.imread(file_path)
-                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    fixed_size = (43, 128)
-                    img = cv2.resize(img, dsize=fixed_size)
-                    train_data.append(img)
-                    train_label.append(1)
-    eval_data.append(train_data[0])
-    eval_label.append(train_label[0])
-    test_data.append(train_data[1])
-    test_label.append(train_label[1])
-    train_data = np.array(train_data, dtype=np.float32) / 255. #, dtype=np.float32)
-    train_label = np.array(train_label, dtype=np.int32) #, dtype=np.int32)
-    eval_data = np.array(eval_data) #, dtype=np.float32)
-    eval_label = np.array(eval_label) #, dtype=np.int32)
-    test_data = np.array(test_data) #, dtype=np.float32)
-    test_label = np.array(test_label) #, dtype=np.int32)
-    print(train_data.shape)
-    return train_data, train_label, eval_data, eval_label, test_data, test_label
+from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+from keras.layers import Dense, Dropout, BatchNormalization, Conv2D, MaxPooling2D, GlobalMaxPool2D
 
 
 def main():
-    train_data = None
-    train_label = None
-    eval_data = None
-    eval_label = None
-    test_data = None
-    test_label = None
-    train_data, train_label, eval_data, eval_label, test_data, test_label = pre_process()
-    epochs = 10
+    train_data = np.load('train_data.npy')
+    train_label = np.load('train_label.npy')
+    eval_data = np.load('eval_data.npy')
+    eval_label = np.load('eval_label.npy')
+    test_data = np.load('test_data.npy')
+    test_label = np.load('test_label.npy')
+    print('Train data: ', len(train_data))
+    print('Eval data: ', len(eval_data))
+    print('Test data: ', len(test_data))
+    epochs = 1000
     learn_rate = 0.001
-    model = Sequential()
-    model.add(BatchNormalization(input_shape=(43, 128, 1)))
-    model.add(Conv2D(32, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(Conv2D(32, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(64, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(Conv2D(64, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(256, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(Conv2D(256, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
-                     kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(GlobalMaxPool2D())
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu', kernel_initializer='glorot_uniform', bias_initializer='zeros'))
-    model.add(Dropout(0.50))
-    model.add(Dense(11, activation='sigmoid'))
-    print(model.summary())
+    if os.path.isfile('cnn_model.h5'):
+        model = load_model('cnn_model.h5')
+    else:
+        model = Sequential()
+        model.add(BatchNormalization(input_shape=(43, 128, 3)))
+        model.add(Conv2D(32, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(Conv2D(32, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(64, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(Conv2D(64, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(128, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(Conv2D(128, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(256, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(Conv2D(256, kernel_size=(3, 3), padding='same', strides=(1, 1), activation='relu',
+                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(GlobalMaxPool2D())
+        model.add(Dense(1024, activation='relu', kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+        model.add(Dropout(0.50))
+        model.add(Dense(11, activation='sigmoid'))
+        print(model.summary())
     opt = optimizers.Adam(lr=learn_rate)
     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-    callbacks = [EarlyStopping(monitor='val_loss', patience=2, verbose=2),
-                 ModelCheckpoint('checkpoint.h5', monitor='val_loss', save_best_only=True, verbose=2)]
-    model.fit(train_data, train_label, batch_size=128, verbose=2, epochs=epochs, callbacks=callbacks)
+    callbacks = [EarlyStopping(monitor='val_loss', patience=5, verbose=2),
+                 ModelCheckpoint('checkpoint.h5', monitor='val_loss', save_best_only=True, verbose=2),
+                 TensorBoard(log_dir='./tensorboard', histogram_freq=3, write_graph=True, write_images=True,
+                             write_grads=True)]
+    model.fit(x=train_data, y=train_label, validation_data=(eval_data, eval_label), batch_size=128, verbose=2,
+              epochs=epochs, callbacks=callbacks, shuffle=True)
     model.save('cnn_model.h5')
-    score = model.evaluate(eval_data, eval_label, batch_size=128)
+    saved_model = load_model('checkpoint.h5')
+    score = saved_model.evaluate(x=eval_data, y=eval_label, batch_size=128)
     print(score)
-    saved_model = load_model('cnn_model.h5')
     predictions = saved_model.predict(test_data, batch_size=128)
     print(predictions)
+    print(test_label)
 
 
 if __name__ == '__main__':
---
+++ CNN/data_process.py
@@ -0,0 +1,65 @@
+import os
+import cv2
+import numpy as np
+
+
+def main():
+    filecount = 0
+    train_data = []
+    train_label = []
+    eval_data = []
+    eval_label = []
+    test_data = []
+    test_label = []
+    label_dict = {"cel": 0, "cla": 1, "flu": 2, "gac": 3, "gel": 4, "org": 5, "pia": 6, "sax": 7, "tru": 8, "vio": 9,
+                  "voi": 10}
+    path = "/Users/praneet/Documents/USC - MS/Fall 2017/CS 599 - DL/CSCI-599-DL/CNN/IRMAS-training_spectrograms"
+    for root, dirs, files in os.walk(path):
+        for file_name in files:
+            if file_name.endswith(".png"):
+                filecount += 1
+                file_path = os.path.abspath(os.path.join(root, file_name))
+                img = cv2.imread(file_path)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                fixed_size = (128, 43)
+                img = cv2.resize(img, dsize=fixed_size)
+                # cv2.imshow('Image', img)
+                # cv2.waitKey(0)
+                # cv2.destroyAllWindows()
+                # print(root)
+                print(file_name)
+                file_label = root.split('/')[-1]
+                train_data.append(img)
+                tmp = [0] * 11
+                tmp[label_dict[file_label]] = 1
+                train_label.append(tmp)
+                if filecount % 7 == 0:
+                    eval_data.append(img)
+                    tmp = [0] * 11
+                    tmp[label_dict[file_label]] = 1
+                    eval_label.append(tmp)
+                if filecount % 11 == 0:
+                    test_data.append(img)
+                    tmp = [0] * 11
+                    tmp[label_dict[file_label]] = 1
+                    test_label.append(tmp)
+    print(len(train_data), len(train_label), len(eval_data), len(eval_label), len(test_data), len(test_label))
+    train_data = np.array(train_data, np.float32) / 255.
+    train_label = np.array(train_label, np.int32)
+    eval_data = np.array(eval_data, np.float32) / 255.
+    eval_label = np.array(eval_label, np.int32)
+    test_data = np.array(test_data, np.float32) / 255.
+    test_label = np.array(test_label, np.int32)
+    print(train_data.shape, train_label.shape)
+    print(eval_data.shape, eval_label.shape)
+    print(test_data.shape, test_label.shape)
+    np.save('train_data', train_data)
+    np.save('train_label', train_label)
+    np.save('eval_data', eval_data)
+    np.save('eval_label', eval_label)
+    np.save('test_data', test_data)
+    np.save('test_label', test_label)
+
+
+if __name__ == '__main__':
+    main()
